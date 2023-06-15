a02547a121c5e6837221e7888cac4ef0a8ead6aa
cyber-idiot <136726700+cyber-idiot@users.noreply.github.com>
2023-06-15 20:09:36 +0200

    Update setup-pi4.sh

--- setup-pi4.sh
+++ setup-pi4.sh
@@ -15,7 +15,8 @@ echo "Installing required tools" ; sleep 1
 sudo apt-get install libsndfile1-dev zenity git -y
 clear
 echo "Changing directory --> src" ; sleep 1
-cd /home/pi/PiFM/src
+username=$(whoami)
+cd /home/$username/PiFM/src
 clear
 echo "Cleaning up" ; sleep 1
 make clean
@@ -29,26 +30,26 @@ echo "Editing -> /boot/config.txt" ; sleep 1
 echo "gpu_freq=250" | sudo tee -a /boot/config.txt > /dev/null
 clear
 echo "Adding software" ; sleep 1
-cd /home/pi/PiFM
+cd /home/$username/PiFM
 # Copy GUI Version
-sudo cp /home/pi/PiFM/src/pi4/pifm.sh /usr/local/bin
+sudo cp /home/$username/PiFM/src/pi4/pifm.sh /usr/local/bin
 sudo mv /usr/local/bin/pifm.sh /usr/local/bin/pifm
 # Copy Basic Version
-sudo cp /home/pi/PiFM/src/pi4/pifm-basic.sh /usr/local/bin
+sudo cp /home/$username/PiFM/src/pi4/pifm-basic.sh /usr/local/bin
 sudo mv /usr/local/bin/pifm-basic.sh /usr/local/bin/pifm-basic
 clear
 echo "Creating shortcuts" ; sleep 1
-sudo cp /home/pi/PiFM/src/pi4/PiFM.desktop /home/pi/Desktop
-sudo cp /home/pi/PiFM/src/pi4/PiFM.desktop /usr/share/applications
+sudo cp /home/$username/PiFM/src/pi4/PiFM.desktop /home/$username/Desktop
+sudo cp /home/$username/PiFM/src/pi4/PiFM.desktop /usr/share/applications
 echo "Changing filename/type" ; sleep 0.1
-sudo mv /home/pi/PiFM/src/pi_fm_adv /home/pi/PiFM/src/pifm
+sudo mv /home/$username/PiFM/src/pi_fm_adv /home/$username/PiFM/src/pifm
 clear
 echo "Patching xterm" ; sleep 0.1
 cd /usr/bin
 sudo cp lxterminal xterm
 clear
 echo "Assigning permissions" ; sleep 0.1
-sudo chmod +x /home/pi/PiFM/pifm
+sudo chmod +x /home/$username/PiFM/pifm
 sudo chmod +x /usr/local/bin/pifm
 sudo chmod +x /usr/local/bin/pifm-basic
 clear
